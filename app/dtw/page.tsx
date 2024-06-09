2de229d33b330af6591e6eb36c9e8ba46e5d7832
nugrhx <nugrhx@gmail.com>
2024-06-09 20:57:41 +0700

    update

--- app/dtw/page.tsx
+++ app/dtw/page.tsx
@@ -9,38 +9,72 @@ export default async function Home() {
 
   return (
     <>
-        <div className="grid w-full max-w-screen-xl animate-fade-up grid-cols-1 gap-5 px-5 md:grid-cols-3 xl:px-0 z-10 -mt-12">
-        {features.map(({ title, description, demo, large }) => (
-          <Card
-            title={title}
-            key={title}
-            description={description}
-            demo={
-              title === "Beautiful, reusable components" ? (
-                <ComponentGrid />
-              ) : (
-                demo
-              )
-            }
-            large={large}
-          />
-        ))}
+        
+      <div className="bg-gray-100">
+        <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
+          <div className="mx-auto max-w-2xl py-16 sm:py-24 lg:max-w-none lg:py-32">
+            <h2 className="text-2xl font-bold text-gray-900">Collections</h2>
+
+            <div className="mt-6 space-y-12 lg:grid lg:grid-cols-3 lg:gap-x-6 lg:space-y-0">
+              <div className="group relative">
+                <div className="relative h-80 w-full overflow-hidden rounded-lg bg-white sm:aspect-h-1 sm:aspect-w-2 lg:aspect-h-1 lg:aspect-w-1 group-hover:opacity-75 sm:h-64">
+                  <Image
+                    src="/landing.jpg" 
+                    width={1080}
+                    height={1080}
+                    alt="Desk with leather desk pad, walnut desk organizer, wireless keyboard and mouse, and porcelain mug." 
+                    className="h-full w-full object-cover object-center"
+                  />
+                </div>
+                <h3 className="mt-6 text-sm text-gray-500">
+                  <a href="#">
+                    <span className="absolute inset-0"></span>
+                    Desk and Office
+                  </a>
+                </h3>
+                <p className="text-base font-semibold text-gray-900">Work from home accessories</p>
+              </div>
+              <div className="group relative">
+                <div className="relative h-80 w-full overflow-hidden rounded-lg bg-white sm:aspect-h-1 sm:aspect-w-2 lg:aspect-h-1 lg:aspect-w-1 group-hover:opacity-75 sm:h-64">
+                  <Image
+                    src="/landing.jpg" 
+                    width={1080}
+                    height={1080}
+                    alt="Wood table with porcelain mug, leather journal, brass pen, leather key ring, and a houseplant." 
+                    className="h-full w-full object-cover object-center"
+                    />
+                </div>
+                <h3 className="mt-6 text-sm text-gray-500">
+                  <a href="#">
+                    <span className="absolute inset-0"></span>
+                    Self-Improvement
+                  </a>
+                </h3>
+                <p className="text-base font-semibold text-gray-900">Journals and note-taking</p>
+              </div>
+              <div className="group relative">
+                <div className="relative h-80 w-full overflow-hidden rounded-lg bg-white sm:aspect-h-1 sm:aspect-w-2 lg:aspect-h-1 lg:aspect-w-1 group-hover:opacity-75 sm:h-64">
+                  <Image
+                    src="/landing.jpg" 
+                    width={1080}
+                    height={1080}
+                    alt="Collection of four insulated travel bottles on wooden shelf." 
+                    className="h-full w-full object-cover object-center"
+                   />
+                </div>
+                <h3 className="mt-6 text-sm text-gray-500">
+                  <a href="#">
+                    <span className="absolute inset-0"></span>
+                    Travel
+                  </a>
+                </h3>
+                <p className="text-base font-semibold text-gray-900">Daily commute essentials</p>
+              </div>
+            </div>
+          </div>
         </div>
+      </div>
+
     </>
   );
 }
-
-const features = [
-  {
-    title: "Beautiful, reusable components",
-    description:
-      "Pre-built beautiful, a11y-first components, powered by [Tailwind CSS](https://tailwindcss.com/), [Radix UI](https://www.radix-ui.com/), and [Framer Motion](https://framer.com/motion)",
-    large: true,
-  },
-  {
-    title: "Performance first",
-    description:
-      "Built on [Next.js](https://nextjs.org/) primitives like `@next/font` and `next/image` for stellar performance.",
-    demo: <WebVitals />,
-  },
-];

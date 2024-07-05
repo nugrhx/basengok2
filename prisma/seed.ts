becf1ab91ae8e4c2e1e849fbcbebc6d2850c242b
nugrhx <nugrhx@gmail.com>
2024-07-05 07:47:43 +0700

    fix

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -18,18 +18,18 @@ import { PrismaClient } from '@prisma/client';
 const prisma = new PrismaClient();
 
 async function seed() {
-  try {
-    await prisma.admin.createMany({ data: adminData });
-    await prisma.amenitas.createMany({ data: amenitasData });
-    await prisma.pendukung.createMany({ data: pendukungData });
-    await prisma.dtw.createMany({ data: dtwData });
-    await prisma.user.createMany({ data: userData });
-    // ... insert data for other tables
-  } catch (error) {
-    console.error('Error seeding data:', error);
-  } finally {
-    await prisma.$disconnect();
-  }
+  // try {
+  //   await prisma.admin.createMany({ data: adminData });
+  //   await prisma.amenitas.createMany({ data: amenitasData });
+  //   await prisma.pendukung.createMany({ data: pendukungData });
+  //   await prisma.dtw.createMany({ data: dtwData });
+  //   await prisma.user.createMany({ data: userData });
+  //   // ... insert data for other tables
+  // } catch (error) {
+  //   console.error('Error seeding data:', error);
+  // } finally {
+  //   await prisma.$disconnect();
+  // }
 }
 
 seed();
